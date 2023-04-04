43dcea150b48c2d92d5d0ff3b1658ef61a817e5f
Ruben Viana <up202005108@up.pt>
2023-04-04 23:12:20 +0100

    comments added

--- code25/piece.py
+++ code25/piece.py
@@ -1,41 +1,55 @@
-from .constants import RED, WHITE, SQUARE_SIZE, OFFSET, BLUE, WHITE, WHITE_PIECE, BLUE_PIECE
+# Import constants used in the class
+from .constants import RED, WHITE, SQUARE_SIZE, OFFSET, BLUE, WHITE_PIECE, BLUE_PIECE
+# Import the Pygame library
 import pygame
 
+# Define a class to represent a checkers piece
 class Piece:
+    # Define class-level constants
     PADDING = 15
     OUTLINE = 3
 
+    # Define the constructor method for the Piece class
     def __init__(self, row, col, color):
+        # Set the row, column, and color attributes of the piece
         self.row = row
         self.col = col
         self.color = color
+        # Set a flag indicating whether the piece has become a king
         self.goal = False
+        # Calculate the initial position of the piece on the board
         self.x = OFFSET
         self.y = OFFSET
         self.calc_pos()
 
+    # Method to calculate the position of the piece on the board
     def calc_pos(self):
         self.x = OFFSET + SQUARE_SIZE * self.col + (SQUARE_SIZE // 2)
         self.y = OFFSET + SQUARE_SIZE * self.row + (SQUARE_SIZE // 2) 
 
+    # Method to set the "goal" flag of the piece
     def make_goal(self):
         self.goal = True
     
+    # Method to draw the piece on the board
     def draw(self, win):
+        # Calculate the radius of the piece
         radius = SQUARE_SIZE//2 - self.PADDING
+        # If the piece is white, draw a white game piece image at its position
         if self.color == WHITE:
-            #pygame.draw.circle(win, BLUE, (self.x, self.y), radius + self.OUTLINE)
             win.blit(WHITE_PIECE, (OFFSET + SQUARE_SIZE * self.col + 12, OFFSET + SQUARE_SIZE * self.row + 12))
+        # If the piece is blue, draw a blue game piece image at its position
         elif self.color == BLUE:
-            #pygame.draw.circle(win, WHITE, (self.x, self.y), radius + self.OUTLINE)
             win.blit(BLUE_PIECE, (OFFSET + SQUARE_SIZE * self.col + 12, OFFSET + SQUARE_SIZE * self.row + 12))
-        #pygame.draw.circle(win, self.color, (self.x, self.y), radius)
-        
 
+    # Method to move the piece to a new position on the board
     def move(self, row, col):
+        # Update the row and column attributes of the piece
         self.row = row
         self.col = col
+        # Calculate the new position of the piece on the board
         self.calc_pos()
 
+    # Method to return a string representation of the piece
     def __repr__(self):
         return str(self.color)
--- code25/spot.py
+++ code25/spot.py
@@ -1,35 +1,47 @@
+# import necessary constants and modules
 from .constants import RED, WHITE, SQUARE_SIZE, BLUE, OFFSET, WOOD, WHITE_TILE, BLUE_TILE, BLUE_WHITE_TILE
 import pygame
 
+# define the Spot class
 class Spot:
     def __init__(self, row, col, color):
+        # initialize the row, column, and color attributes of the Spot instance
         self.row = row
         self.col = col
         self.color = color
+        # set initial x and y positions to 0, will be calculated in calc_pos() method
         self.x = 0
         self.y = 0  
+        # calculate the position of the spot on the board based on its row and column
         self.calc_pos() 
 	
     def draw(self, win):
-        #pygame.draw.rect(win, self.color, (self.x, self.y, SQUARE_SIZE, SQUARE_SIZE), 0, 10)
+        # draw the spot on the screen
         if self.color == WHITE:
-             win.blit(WHITE_TILE, (self.x, self.y))
+            # if the spot is white, blit the WHITE_TILE image onto the screen
+            win.blit(WHITE_TILE, (self.x, self.y))
         elif self.color == BLUE:
+            # if the spot is blue, blit the BLUE_TILE image onto the screen
             win.blit(BLUE_TILE, (self.x, self.y))
         else:
-             win.blit(BLUE_WHITE_TILE, (self.x, self.y))
+            # if the spot is neither white nor blue, blit the BLUE_WHITE_TILE image onto the screen
+            win.blit(BLUE_WHITE_TILE, (self.x, self.y))
+        # draw a rectangle around the spot with a border of 1 pixel and the WOOD color
         pygame.draw.rect(win, WOOD, (self.x, self.y, SQUARE_SIZE, SQUARE_SIZE), 1)
         
-
     def calc_pos(self):
+        # calculate the x and y positions of the spot on the screen based on its row and column
         self.x = SQUARE_SIZE * self.col + OFFSET
         self.y = SQUARE_SIZE * self.row + OFFSET
 
     def get_pos(self):
+        # return the row and column position of the spot as a tuple
         return self.row, self.col
 
     def get_color(self):
+        # return the color of the spot
         return self.color
     
     def set_color(self, color):
+        # set the color of the spot to the given value
         self.color = color
